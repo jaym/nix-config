871edb08011a220e79a1dbb0eef8e5187370b5fb
Jay Mundrawala <jay@thechamberofunderstanding.com>
2025-01-01 07:02:30 -0600

    Initial macos configuration

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, darwin, home-manager, nixpkgs, ... }@inputs: {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#momcorp
+    darwinConfigurations."momcorp" = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      pkgs = import inputs.nixpkgs {
+          system = "aarch64-darwin";
+          config.allowUnfree = true;
+      };
+      modules = [
+        ./modules/darwin
+        home-manager.darwinModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.jaym = import ./modules/home-manager;
+          };
+        }
+      ];
+    };
+  };
+}
---
+++ modules/home-manager/default.nix
@@ -0,0 +1,65 @@
+{ pkgs, lib, ...}: {
+    home.username = "jaym";
+    home.homeDirectory = lib.mkForce "/Users/jaym";
+
+    home.stateVersion = "24.11";
+    home.packages = with pkgs; [
+        curl
+        fd
+        ffmpeg
+        less
+        ripgrep
+        go
+    ];
+    home.sessionVariables = {
+        PAGER = "less";
+        CLICLOLOR = 1;
+        EDITOR = "nvim";
+        GOPATH = "$HOME/go";
+    };
+
+    programs.bat = {
+        enable = true;
+        config.theme = "TwoDark";
+    };
+
+    programs.fzf = {
+        enable = true;
+    };
+
+    programs.eza = {
+        enable = true;
+        icons = "auto";
+    };
+
+    programs.git = {
+        enable = true;
+        userName = "Jay Mundrawala";
+        userEmail = "jay@thechamberofunderstanding.com";
+    };
+
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestion = {
+            enable = true;
+        };
+        syntaxHighlighting = {
+            enable = true;
+        };
+        shellAliases = {
+            vim = "nvim";
+            nixswitch = "darwin-rebuild switch --flake ~/nix-config/.#";
+            nixup = "pushd ~/nix-config; nix flake update; nixswitch; popd";
+        };
+    };
+
+    programs.starship = {
+        enable = true;
+        enableZshIntegration = true;
+    };
+
+    home.file = {
+        ".aerospace.toml".source = ./dotfiles/aerospace.toml;
+    };
+}
